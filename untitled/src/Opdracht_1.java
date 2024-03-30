e605eb2c7a23d4e98d0186b4c8dfa2679201e0a5
IceKowedd <siebestienstra@gmail.com>
2024-03-30 23:02:39 +0100

    Update

--- untitled/src/Opdracht_1.java
+++ untitled/src/Opdracht_1.java
@@ -1,39 +1,36 @@
 import java.util.HashMap;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 
 public class Opdracht_1 {
 
+
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
+    // Main Function/Method > number array is created > even and un-even hashmap created.
     System.out.println("Hello and welcome!");
-    int[] mijnNummers = {42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8};
+    int[] myNumbers = {42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8};
+    HashMap<String, Integer> countUnEven = new HashMap<>();
+    countUnEven.put("even", 0);
+    countUnEven.put("uneven", 0);
 
-    HashMap<String, Integer> tellerEvenOneven = new HashMap<>();
-    tellerEvenOneven.put("even", 0);
-    tellerEvenOneven.put("oneven", 0);
-
-    for (int i = 0; i < mijnNummers.length; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    // Loops over myNumbers array > checks and prints if even or un-even number and increments even/un-even hashmap. > prints total in the end.
+    for (int i = 0; i < myNumbers.length; i++) {
       System.out.print("getal op Index " + i);
-      if (mijnNummers[i] % 2 == 0) {
-        tellerEvenOneven.put("even", tellerEvenOneven.get("even") + 1);
-        if (tellerEvenOneven.get("even") >= 2) {
-        System.out.println(": " + mijnNummers[i] + " is een even getal, we hebben nu " + tellerEvenOneven.get("even") + " even getallen");
+      if (myNumbers[i] % 2 == 0) {
+        countUnEven.put("even", countUnEven.get("even") + 1);
+        if (countUnEven.get("even") >= 2) {
+        System.out.println(": " + myNumbers[i] + " is een even getal, we hebben nu " + countUnEven.get("even") + " even getallen");
       } else {
-        System.out.println(": " + mijnNummers[i] + " is een even getal, we hebben nu " + tellerEvenOneven.get("even") + " even getal");
+        System.out.println(": " + myNumbers[i] + " is een even getal, we hebben nu " + countUnEven.get("even") + " even getal");
       }
     } else {
-        tellerEvenOneven.put("oneven", tellerEvenOneven.get("oneven") + 1);
-        if (tellerEvenOneven.get("oneven") >= 2) {
-          System.out.println(": " + mijnNummers[i] + " is een oneven getal, we hebben nu " + tellerEvenOneven.get("oneven") + " oneven getallen");
+        countUnEven.put("uneven", countUnEven.get("uneven") + 1);
+        if (countUnEven.get("uneven") >= 2) {
+          System.out.println(": " + myNumbers[i] + " is een oneven getal, we hebben nu " + countUnEven.get("uneven") + " oneven getallen");
         } else {
-          System.out.println(": " + mijnNummers[i] + " is een oneven getal, we hebben nu " + tellerEvenOneven.get("oneven") + " oneven getal");
+          System.out.println(": " + myNumbers[i] + " is een oneven getal, we hebben nu " + countUnEven.get("uneven") + " oneven getal");
         }
       }
     }
     System.out.println();
-    System.out.println("we hebben dus totaal " + tellerEvenOneven.get("even") + " even getallen en " + tellerEvenOneven.get("oneven") + " on even getallen");}
+    System.out.println("we hebben dus totaal " + countUnEven.get("even") + " even getallen en " + countUnEven.get("uneven") + " oneven getallen");}
 }
